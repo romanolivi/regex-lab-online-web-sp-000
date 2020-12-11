a22f2d98e83a4aed55d529f6ef15c412abc989ad
Romano Livi <romano_livi@outlook.com>
2020-12-11 03:41:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,4 @@
 def starts_with_a_vowel?(word)
-
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,7 +6,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  text.grep/^\w{5}\s/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
